bbe462aa3d22e00f651ad5cb540fbc98e594feba
Sam Denty <sam@samdenty.com>
2025-04-27 14:18:53 +0100

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,28 +8,28 @@ import { TRACKING_ID } from "./_document";
 import { BackgroundEffect } from "../src/components/BackgroundEffect";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(() => new QueryClient());
-  const router = useRouter();
+	const [queryClient] = useState(() => new QueryClient());
+	const router = useRouter();
 
-  const handleRouteChange = (url: string) => {
-    (window as any).gtag("config", TRACKING_ID, {
-      page_path: url,
-    });
-  };
+	const handleRouteChange = (url: string) => {
+		(window as any).gtag("config", TRACKING_ID, {
+			page_path: url,
+		});
+	};
 
-  useEffect(() => {
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+	useEffect(() => {
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <BackgroundEffect />
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Hydrate state={pageProps.dehydratedState}>
+				<BackgroundEffect />
+				<Component {...pageProps} />
+			</Hydrate>
+		</QueryClientProvider>
+	);
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,33 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/next";
 
 export const TRACKING_ID = "UA-85426772-4";
 
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`}
-        />
+	return (
+		<Html>
+			<Head>
+				<script
+					async
+					src={`https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`}
+				/>
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${TRACKING_ID}', { page_path: window.location.pathname });
             `,
-          }}
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+					}}
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+				<Analytics />
+			</body>
+		</Html>
+	);
 }
